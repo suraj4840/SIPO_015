5fa7f273cbe4d1f26b87f4cb458e9294e4fe455f
suraj4840 <144480393+suraj4840@users.noreply.github.com>
2023-09-25 19:15:09 +0530

    Create dff.v

---
+++ dff.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/22/2023 09:13:37 PM
+// Design Name: 
+// Module Name: D_ff
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module D_ff(clk,rst,d,q);
+input clk,rst,d;
+output reg q;
+
+always @(posedge clk,posedge rst)
+begin
+      if(rst)
+          q<= 1'b0;
+      else 
+          q<=d;
+end 
+             
+endmodule
